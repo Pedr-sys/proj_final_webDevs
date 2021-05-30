d74b19370833903e85b6368dba806105facad293
Pedro Sak <pedsakxxi82@yahoo.com>
2021-05-30 00:29:47 -0300

    criar a comidinha

--- index.html/script.js
+++ index.html/script.js
@@ -9,10 +9,14 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
+} 
 
 function criarBG(){
    context.fillStyle = "#5cffff";
-   context.fillRect(0,0,16 * box,16 * box);
+   context.fillRect(0, 0, 16 * box, 16 * box);
 
 }
 
@@ -24,13 +28,18 @@ function criarCobrinha(){
 	}
 }
 
-document.addEventListener('Keydown', update);
+function drawFood(){
+	context.fillStyle = "red";
+	context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener("Keydown", update);
 
 function update (event){
 	if(event.keyCode == 37 && direction != "right") direction = "left";
 	if(event.KeyCode == 38 && direction != "down") direction = "up";
 	if(event.Keycode == 39 && direction != "left") direction = "right";
-	if(event.Keycode == 40 && direction !== "up") direction = "down";
+	if(event.Keycode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo() {
@@ -41,6 +50,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
