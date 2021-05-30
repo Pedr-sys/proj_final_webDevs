7faedf46477683163e70fbc395e330d38f056e5c
Pedro Sak <pedsakxxi82@yahoo.com>
2021-05-29 23:08:06 -0300

    criar cobrinha

--- index.html/script.js
+++ index.html/script.js
@@ -1,13 +1,29 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
 
-function criarBG() {
-   context.fillStyle ="Lightgreen";
-   context.fillRect(0, 0, 16 * box, 16 * box);
+	x: 8 * box,
+	y: 8 * box
+}
+
+function criarBG(){
+   context.fillStyle = "#5cffff";
+   context.fillRect(0,0,16 * box,16 * box);
 
 }
 
-criarBG() {
+function criarCobrinha(){
+	for(i=0; i < snake.length; i++){
 
+		context.fillStyle = "#240000";
+		context.fillRect(snake[i].x, snake[i].y, box, box);
+	}
 }
+
+
+criarBG();
+criarCobrinha();
+
+
